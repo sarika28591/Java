d64c1201f0e498289661f4caaa069a9c85ea05a2
sarika28591 <sarikamulay28@gmail.com>
2018-04-06 12:18:26 +0530

    first commit

---
+++ variable.java
@@ -0,0 +1,39 @@
+package hello;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char myCharacter;
+		myCharacter = 'a';
+		System.out.println(myCharacter);
+		myCharacter = 'b';
+		System.out.println(myCharacter);
+	
+		int ourNumber;
+		ourNumber = 0;
+		System.out.println(ourNumber);
+		ourNumber = 2 + 2;
+		System.out.println(ourNumber);
+		
+		double decimalNumber = 10.15;
+		System.out.println(decimalNumber);
+		
+		String myString = "sarika";
+		System.out.println(myString);
+		
+		int Number;
+		int newVariable = 10 + 15;
+		int anotherVariable = 10 - newVariable;
+		System.out.println(anotherVariable);
+		
+		boolean user = false;
+         if ( user == false) {
+		System.out.println("it's true");
+		}
+		else {
+		System.out.println("it's false");
+		} 
+	}
+
+}
